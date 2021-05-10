331d6ace2ca32398a9d4328cc141f108444bf918
coderdjc <djc1997717@vip.qq.com>
2021-05-10 15:48:16 +0800

    Initial commit

---
+++ src/views/home/childComps/HomeRecommendView.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="recommend">
+        <div v-for="item in recommends" :key='item.link' class="recommend-item">
+            <a :href="item.link"><img :src="item.image" alt="">
+                <div>{{item.title}}</div>
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HomeRecommendView',
+        props: {
+            recommends: {
+                typeof: Array,
+                default() {
+                    return []
+                }
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+.recommend {
+    display: flex;
+    width: 100%;
+    text-align: center;
+    font-size: 12px;
+    padding: 10px 0 20px;
+    border-bottom: 10px solid #eee;
+}
+.recommend-item {
+flex: 1;
+}
+.recommend img {
+    width: 80%;
+    margin-top: 10px;
+}
+</style>
